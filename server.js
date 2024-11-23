0bbbeeadcf230504a23f18142d270fc749e902a4
Kenza <kenzalimantara05@gmail.com>
2024-11-23 12:21:46 +0700

    update

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log('API FoodScape');
+import Hapi from '@hapi/hapi';
+
+const server = Hapi.server({
+    port: 9000,
+    host: 'localhost',
+});
+
+const init = async () => {
+    await server.start();
+    console.log(`Server berjalan di http://localhost:9000`);
+    console.log('API FoodScape');
+};
+
+init();
