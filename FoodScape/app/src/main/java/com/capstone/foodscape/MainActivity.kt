5176114cc056d5cbbf9937467c00ab80d561d3ff
Nisa Arizka <nisarizkarany@gmail.com>
2024-11-30 14:33:54 +0700

    perubahan layout

--- FoodScape/app/src/main/java/com/capstone/foodscape/MainActivity.kt
+++ FoodScape/app/src/main/java/com/capstone/foodscape/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var bind: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
 
         val handler = Handler()
         handler.postDelayed({
